7f2ea569442d3477ca70bb91c6c9ca4bb2d1f15a
BloodYce <mattyago99@gmail.com>
2024-12-06 00:01:46 +0100

    Partial solution/003 - read_files, concatenate_files OK | clean_dataset PARTIAL

---
+++ projects/003-concatenate-and-cleanup-multiple-files/python/main.py
@@ -0,0 +1,67 @@
+'''
+In this exercise, you will practice concatenating data from multiple text files and cleaning up the combined dataset.   
+You'll work with a set of text files containing information about products and their attributes.   
+Your task is to concatenate these files into a single dataset and perform some basic data cleaning tasks.
+
+Examine the structure of the text files to understand how the data is organized.
+
+Define functions:
+- **read files**, the function will take care of read the content of each text file and store it in a 
+    list or another appropriate data structure. Accepts the path as input.
+
+
+- **concatenate files**, the function will take care of concatenate the content from all the text files 
+    to create a single combined dataset.
+
+
+- **clean dataset**, the function will take care of performing the following data cleaning tasks:
+    - Remove any duplicate lines.
+    - Remove any lines with missing or incomplete information.
+    - Remove any leading or trailing whitespace from each line.
+
+
+- **save file**, the function will take care of save the cleaned and concatenated dataset to a new text file
+    named combined_products.txt including a single header and with each value separated with a comma.
+'''
+
+import filecmp
+from difflib import Differ
+
+def read_files(file, file2):
+    list = file.readlines()
+    list2 = file2.readlines()
+    stringlist = ''.join(list)
+    stringlist2 = ''.join(list2)
+    return stringlist, stringlist2
+
+def concatenate_files(file, file2):
+    list = file.readlines()
+    list2 = file2.readlines()
+    concatenateddata = list + list2
+    concatenateddatastring = ''.join(concatenateddata)
+    return concatenateddatastring
+
+def clean_dataset(file, file2):
+    list = file.readlines()
+    list2 = file2.readlines()
+    concatenateddata = list + list2
+    newlist = []
+    for line in concatenateddata:
+        if line not in newlist:
+            newlist.append(concatenateddata)
+    return concatenateddata
+    
+#def save_file():
+
+
+def main():
+    file = open('projects/003-concatenate-and-cleanup-multiple-files/python/example.txt', 'r')
+    file2 = open('projects/003-concatenate-and-cleanup-multiple-files/python/example 2.txt', 'r')
+    #stringlist, stringlist2 = read_files(file, file2)
+    #print(stringlist, "\n",stringlist2)
+    #print(concatenate_files(file, file2))
+    print(clean_dataset(file, file2))
+    file.close()
+
+if __name__ == '__main__':
+    main()
