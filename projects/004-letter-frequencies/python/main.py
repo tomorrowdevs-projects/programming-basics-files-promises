a9db9ef9b3d39dd86723cc8ea2f3d39f7b8a5df1
BloodYce <mattyago99@gmail.com>
2024-12-11 00:53:19 +0100

    Complete solution-m6/004

--- projects/004-letter-frequencies/python/main.py
+++ projects/004-letter-frequencies/python/main.py
@@ -1,1 +1,22 @@
-import
+'''The project allows users to analyze the frequency of letters in a given text or document.    
+The program will read input from the user, analyze the frequency of each letter (both uppercase and lowercase), 
+and present the results in a visually appealing format.'''
+
+def analyze_frequency(text, array, letters, counter):
+    uppertext = text.upper()
+    for letter in uppertext:
+        if letter in letters:
+            if letter not in array:
+                array.append(letter)
+                counter += 1
+    return counter
+
+def main():
+    array = []
+    text = input("Please input a text: ")
+    counter = 0
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(analyze_frequency(text, array, letters, counter))
+
+if __name__ == '__main__':
+    main()
